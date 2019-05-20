94be217cd1f0722d3ea45f55e9a67b76b8bb3ef0
Pulkit <pulkit.gupta@thoughtworks.com>
2019-05-20 13:03:05 +0000

    Update Friend.java

--- src/main/java/com/tw/beach/splitwise/entity/Friend.java
+++ src/main/java/com/tw/beach/splitwise/entity/Friend.java
@@ -1,13 +1,9 @@
 package com.tw.beach.splitwise.entity;
-
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Objects;
 
 @Entity
-@Table(name = "Friend")
+@Table(name="friend")
 public class Friend {
     @Id
     @NotNull
@@ -17,138 +13,66 @@ public class Friend {
 
     @NotNull
     @Column(name = "name")
-    private String name;
-
+    private final String name;
+    
     @NotNull
-    @Column(name = "amount_to_pay")
-    private Double amountToPay;
-
+    @ManyToMany(cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.DETACH, CascadeType.PERSIST,CascadeType.REFRESH})
+	@JoinTable(
+	          name = "paid_for_friend",
+	          joinColumns = {@JoinColumn(name = "friend_id")},
+	          inverseJoinColumns = {@JoinColumn(name = "paid_for_id")}
+			  )
+	private PaidFor paidForBill;
+    
     @NotNull
-    @Column(name = "amount_paid")
-    private Double amountPaid;
+    @ManyToMany(cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.DETACH, CascadeType.PERSIST,CascadeType.REFRESH})
+	@JoinTable(
+	          name = "paid_by_friend",
+	          joinColumns = {@JoinColumn(name = "friend_id")},
+	          inverseJoinColumns = {@JoinColumn(name = "paid_by_id")}
+			  )
+	private PaidBy paidByBill;
 
-    @ManyToMany(cascade = {CascadeType.ALL})
-    @JoinTable(
-            name = "Bill_Friend",
-            joinColumns = {@JoinColumn(name = "fiend_id")},
-            inverseJoinColumns = {@JoinColumn(name = "bill_id")}
-    )
-    private List<Bill> bill;
+	public Long getFriendId() {
+		return friendId;
+	}
 
-    @OneToMany(cascade = CascadeType.ALL)
-    @JoinColumn(name = "to_be_paid_by")
-    private List<SettlementAmount> settlementAmounts;
+	public void setFriendId(Long friendId) {
+		this.friendId = friendId;
+	}
 
-    public Friend() {
+	public PaidFor getPaidForBill() {
+		return paidForBill;
+	}
 
-    }
+	public void setPaidForBill(PaidFor paidForBill) {
+		this.paidForBill = paidForBill;
+	}
 
-    public Friend(String name, Double amountPaid, Double amountToPay) {
-        this.name = name;
-        this.amountPaid = amountPaid;
-        this.amountToPay = amountToPay;
-        this.settlementAmounts = new LinkedList<>();
-        this.bill = new LinkedList<>();
-    }
+	public PaidBy getPaidByBill() {
+		return paidByBill;
+	}
 
-    public Long getFriendId() {
-        return friendId;
-    }
+	public void setPaidByBill(PaidBy paidByBill) {
+		this.paidByBill = paidByBill;
+	}
 
-    public void setFriendId(Long friendId) {
-        this.friendId = friendId;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public Friend(@NotNull Long friendId, @NotNull String name, @NotNull PaidFor paidForBill,
+			@NotNull PaidBy paidByBill) {
+		this.friendId = friendId;
+		this.name = name;
+		this.paidForBill = paidForBill;
+		this.paidByBill = paidByBill;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	@Override
+	public String toString() {
+		return "Friend [friendId=" + friendId + ", name=" + name + ", paidForBill=" + paidForBill + ", paidByBill="
+				+ paidByBill + "]";
+	}
 
-    public Double getAmountToPay() {
-        return amountToPay;
-    }
-
-    public void setAmountToPay(Double amountToPay) {
-        this.amountToPay = amountToPay;
-    }
-
-    public Double getAmountPaid() {
-        return amountPaid;
-    }
-
-    public void setAmountPaid(Double amountPaid) {
-        this.amountPaid = amountPaid;
-    }
-
-    public List<Bill> getBill() {
-        return bill;
-    }
-
-    public void setBill(List<Bill> bill) {
-        this.bill = bill;
-    }
-
-    public List<SettlementAmount> getSettlementAmounts() {
-        return settlementAmounts;
-    }
-
-    public void setSettlementAmounts(List<SettlementAmount> settlementAmounts) {
-        this.settlementAmounts = settlementAmounts;
-    }
-
-    public List<SettlementAmount> getSettlementAmount() {
-        return settlementAmounts;
-    }
-
-    void updateAmountToPay(Double amount) {
-        this.amountToPay += amount;
-    }
-
-    void settlementAmountWith(Friend friend, Double amount) {
-        this.amountToPay = this.amountToPay - amount;
-        this.addSettlementAmount(new SettlementAmount(amount, friend));
-    }
-
-    void receiveAmountFrom(Friend friend, Double amount) {
-        this.amountPaid = this.amountPaid - amount;
-        friend.addSettlementAmount(new SettlementAmount(amount, this));
-    }
-
-    Double calculatePaidAndToPayDifference() {
-        return this.amountPaid - this.amountToPay;
-    }
-
-    private void addSettlementAmount(SettlementAmount settlementAmount) {
-        this.settlementAmounts.add(settlementAmount);
-    }
-
-    int compare(Friend friend) {
-        Integer finalAmount = (int) Math.abs(friend.amountPaid - friend.amountToPay);
-        Integer anotherFinalAmount = (int) Math.abs(this.amountPaid - this.amountToPay);
-
-        return anotherFinalAmount - finalAmount;
-    }
-
-    @Override
-    public String toString() {
-        return this.name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Friend friend = (Friend) o;
-        return Objects.equals(name, friend.name) &&
-                Objects.equals(amountToPay, friend.amountToPay) &&
-                Objects.equals(amountPaid, friend.amountPaid);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name);
-    }
 }
